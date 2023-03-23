6ff40b314dc4bade18af198ff40fb5353a524fa8
ecarloslr <ecarloslr@users.noreply.github.com>
2023-03-22 19:58:22 -0600

    First implementation attempt
    - Added Jackson library
    - Implemented getUsers

--- src/main/java/me/nefti/learning/client/ApiClient.java
+++ src/main/java/me/nefti/learning/client/ApiClient.java
@@ -1,16 +1,42 @@
 package me.nefti.learning.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import me.nefti.learning.client.model.UserList;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 /**
  * Client for REST API.
  */
 public class ApiClient {
 
+    private final String url;
+
+    private final int timeoutMillis;
+
+    private final HttpClient httpClient;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
     public ApiClient(String serviceUrl, int timeoutMillis) {
+        this.url = serviceUrl;
+        this.timeoutMillis = timeoutMillis;
+        this.httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.of(timeoutMillis, ChronoUnit.MILLIS))
+                .build();
     }
 
-    public UserList getUserList() {
-        return null;
+    public UserList getUserList() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder().GET()
+                .uri(URI.create(url + "/users"))
+                .build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        return mapper.readValue(response.body(), UserList.class);
     }
 }
